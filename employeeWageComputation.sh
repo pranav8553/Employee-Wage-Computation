349fa297fd7eff038f79465b643cbc8b3ead1dbb
pranav8553 <pranavCIC065@gmail.com>
2020-08-03 19:22:54 +0530

    Added: Calculated PartTime & FullTime wage of employee

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -4,17 +4,17 @@ echo -e "WELCOME TO EMPLOYEE WAGE COMPUTATION PROBLEM\n"
 
 #Constants
 WAGE_PER_HOUR=20
+IS_PART_TIME=1
+IS_FULL_TIME=2
 
-isPresent=1
-randomCheck=$(( RANDOM%2 ))
-
-if [ $randomCheck -eq $isPresent ]
+randomCheck=$(( RANDOM%3 ))
+if [ $randomCheck -eq $IS_PART_TIME ]
 then
-	echo "Employee is Present"
 	empHours=8
+elif [ $randomCheck -eq $IS_FULL_TIME ]
+then
+	empHours=4
 else
-	echo "Employee is Absent"
 	empHours=0
 fi
 dailyWage=$(( $empHours * $WAGE_PER_HOUR ))
-echo "Daily wage: $dailyWage"
