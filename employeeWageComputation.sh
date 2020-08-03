82df51d0aacbfd36276a530af926bf7ea6fa69e8
pranav8553 <pranavCIC065@gmail.com>
2020-08-03 22:00:56 +0530

    Added: Calculated wages till 100 hours and 20 days is reached

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -7,9 +7,16 @@ WAGE_PER_HOUR=20
 IS_PART_TIME=1
 IS_FULL_TIME=2
 DAYS_PER_MONTH=20
+MAXIMUM_WORKING_DAYS=20
+MAXIMUM_WORKING_HOURS=100
 
-for (( day=1; day<=$DAYS_PER_MONTH; day++ ))
+#Variables
+totalWorkingDays=0
+totalWorkingHours=0
+
+while [[ totalWorkingDays -ne $MAXIMUM_WORKING_DAYS && totalWorkingHours -ne $MAXIMUM_WORKING_HOURS ]]
 do
+	((totalWorkingDays++))
 	randomCheck=$(( RANDOM%3 ))
 	case $randomCheck in
 		$IS_PART_TIME)
@@ -22,6 +29,6 @@ do
 			empHours=0
 			;;
 	esac
-	salery=$(( $empHours * $WAGE_PER_HOUR ))
-	totalSalery=$(( $totalSalery + $salery ))
+	totalWorkingHours=$(( $totalWorkingHours + $empHours ))
 done
+totalSalery=$(( $totalWorkingHours * $WAGE_PER_HOUR ))
