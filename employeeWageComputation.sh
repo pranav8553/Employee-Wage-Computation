b0bad081173bae42aa09d2ef7fdbcc7698466dda
pranav8553 <pranavCIC065@gmail.com>
2020-08-03 21:31:33 +0530

    Added: Calculated daily employee wages for a month

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -6,17 +6,22 @@ echo -e "WELCOME TO EMPLOYEE WAGE COMPUTATION PROBLEM\n"
 WAGE_PER_HOUR=20
 IS_PART_TIME=1
 IS_FULL_TIME=2
+DAYS_PER_MONTH=20
 
-randomCheck=$(( RANDOM%3 ))
-case $randomCheck in
-	$IS_PART_TIME)
-		empHours=4
-		;;
-	$IS_FULL_TIME)
-		empHours=8
-		;;
-	*)
-		empHours=0
-		;;
-esac
-dailyWage=$(( $empHours * $WAGE_PER_HOUR ))
+for (( day=1; day<=$DAYS_PER_MONTH; day++ ))
+do
+	randomCheck=$(( RANDOM%3 ))
+	case $randomCheck in
+		$IS_PART_TIME)
+			empHours=4
+			;;
+		$IS_FULL_TIME)
+			empHours=8
+			;;
+		*)
+			empHours=0
+			;;
+	esac
+	salery=$(( $empHours * $WAGE_PER_HOUR ))
+	totalSalery=$(( $totalSalery + $salery ))
+done
